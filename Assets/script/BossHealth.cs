ca23e7aea39380dc332548aad5e9f1755ba2857a
dungntse180117 <dungntse180117@fpt.edu.vn>
2026-02-02 15:36:22 +0700

    Add boss system and health bar UI

---
+++ Assets/script/BossHealth.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BossHealth : MonoBehaviour
+{
+    public float maxHealth = 100f;
+    public float currentHealth = 100f;
+
+    public Image healthBarFill;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        UpdateHealthBar();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(10f);
+        }
+    }
+    public void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+        UpdateHealthBar();
+    }
+
+    void UpdateHealthBar()
+    {
+        if (healthBarFill != null)
+        {
+            healthBarFill.fillAmount = currentHealth / maxHealth;
+        }
+    }
+}
