69e75933cc6001e6082358c2e0ce9caac33cca34
gianguyent525 <gianguyent525@gmail.com>
2026-03-11 16:17:35 +0700

    add changes for weapon upgrade + boss spawn

--- Assets/ProgressBar.cs
+++ Assets/ProgressBar.cs
@@ -5,12 +5,22 @@ public class ProgressBar : MonoBehaviour
 {
     public Slider slider;   // Reference to the UI Slider
     public float fillSpeed = 0.2f;
+    public GameObject boss; // Reference to the boss
+
+    private bool bossSpawned = false;
 
     void Update()
     {
         if (slider.value < slider.maxValue)
         {
             slider.value += Time.deltaTime * fillSpeed;
+        }
+
+        // When progress bar is full, spawn boss
+        if (!bossSpawned && slider.value >= slider.maxValue)
+        {
+            boss.SetActive(true);
+            bossSpawned = true;
         }
     }
 }
